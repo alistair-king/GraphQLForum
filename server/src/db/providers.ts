0a0f036435ba9d945dbfe880f34151a3e0e49b65
Alistair King <mr.alistairk.king@gmail.com>
2020-12-19 16:29:50 +1100

    Adds .env file

--- server/src/db/providers.ts
+++ server/src/db/providers.ts
@@ -7,16 +7,24 @@ import { Thread } from '@server/forums/threads/entity'
 import { Reply } from '@server/forums/threads/reply/entity'
 import { User } from '@server/users/entity'
 
+require('dotenv').config() 
+const {
+  DB_HOST = '',
+  DB_DATABASE = '',
+  DB_USER = '',
+  DB_PASSWORD = ''
+} = process.env
+
 export const databaseProviders = [
   {
     provide: Constants.DATABASE_CONNECTION,
     useFactory: async () => await createConnection({
       type: 'mysql',
-      host: 'localhost',
+      host: DB_HOST,
       port: 3306,
-      username: '',
-      password: '',
-      database: '',
+      database: DB_DATABASE,
+      username: DB_USER,
+      password: DB_PASSWORD,
       entities: [
         Forum,
         Thread,
