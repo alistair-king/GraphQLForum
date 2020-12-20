63c3f7a7a98370a1983861ee4a674930481f12f7
Alistair King <mr.alistairk.king@gmail.com>
2020-12-20 12:01:52 +1100

    Move grapql endpoint to dotenv var

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,7 +14,7 @@ import { ThreadPage } from './pages/threadpage'
 import { NotFoundPage } from './pages/NotFoundPage'
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: process.env.REACT_APP_GRAPHQL_ENDPOINT
 });
 
 
